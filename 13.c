3c708a72b27097262d8f52ab26aee0f8ba9e7ace
2413250743 <2413250743@qq.com>
2021-01-24 20:13:03 +0800

    做了两个题目，这判题系统有一点傻逼

---
+++ 13.c
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char strings[] = {73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33};
+    printf("%s\n",strings);
+    return 0;
+}
